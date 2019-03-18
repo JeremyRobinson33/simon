16e4384457768087fe93357cc7b92eed13bda483
Jeremy Robinson <jrobinson7@pride.hofstra.edu>
2019-03-18 18:49:55 -0400

    Fixed click to start issue

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var level = 0;
 var randomChosenColor;
 var gameStart = false;
 
-$(document).on("click",function () {
+$(document).on("click mousedown",function () {
   if(!gameStart) {
     setH1();
     nextSequence();
